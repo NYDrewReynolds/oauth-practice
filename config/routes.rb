60af5d38c476d48e8524d9b5937be52f84815759
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-07-20 10:52:55 -0600

    Implement Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get  '/auth/github/callback', to: 'sessions#create'
   get  '/auth/github', as: :login
+  delete '/logout', as: :logout, to: 'sessions#destroy'
 
   get  '/dashboard', to: 'dashboard#show'
   root 'home#show'
 end
+
